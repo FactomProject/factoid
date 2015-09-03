86160dc1add85e4c2d4c606355db58cbb7e441e3
ThePiachu <ThePiachu@gmail.com>
2015-09-03 15:23:39 -0700

    Added more conversions.

--- wallet/Conversions.go
+++ wallet/Conversions.go
@@ -1,7 +1,9 @@
 package wallet
 
 import (
+	"encoding/hex"
 	"fmt"
+	"github.com/FactomProject/factoid"
 	"github.com/FactomProject/go-bip32"
 	"github.com/FactomProject/go-bip39"
 	"github.com/btcsuitereleases/btcutil/base58"
@@ -28,6 +30,14 @@ func MnemonicStringToPrivateKey(mnemonic string) ([]byte, error) {
 	return child.Key, nil
 }
 
+func MnemonicStringToPrivateKeyString(mnemonic string) (string, error) {
+	key, err := MnemonicStringToPrivateKey(mnemonic)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", key), nil
+}
+
 func HumanReadableFactoidPrivateKeyToPrivateKey(human string) ([]byte, error) {
 	human = strings.TrimSpace(human)
 	base, version, err := base58.CheckDecode(human)
@@ -42,6 +52,14 @@ func HumanReadableFactoidPrivateKeyToPrivateKey(human string) ([]byte, error) {
 	return base[1:], nil
 }
 
+func HumanReadableFactoidPrivateKeyToPrivateKeyString(human string) (string, error) {
+	key, err := HumanReadableFactoidPrivateKeyToPrivateKey(human)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", key), nil
+}
+
 func HumanReadableECPrivateKeyToPrivateKey(human string) ([]byte, error) {
 	human = strings.TrimSpace(human)
 	base, version, err := base58.CheckDecode(human)
@@ -54,4 +72,66 @@ func HumanReadableECPrivateKeyToPrivateKey(human string) ([]byte, error) {
 	}
 
 	return base[1:], nil
+}
+
+func HumanReadableECPrivateKeyToPrivateKeyString(human string) (string, error) {
+	key, err := HumanReadableECPrivateKeyToPrivateKey(human)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", key), nil
+}
+
+/******************************************************************************/
+/***************************To public key**************************************/
+/******************************************************************************/
+
+func PrivateKeyStringToPublicKeyString(private string) (string, error) {
+	pub, err := PrivateKeyStringToPublicKey(private)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", pub), nil
+}
+
+func PrivateKeyStringToPublicKey(private string) ([]byte, error) {
+	privHex, err := hex.DecodeString(private)
+	if err != nil {
+		return nil, err
+	}
+	return PrivateKeyToPublicKey(privHex)
+}
+
+func PrivateKeyToPublicKey(private []byte) ([]byte, error) {
+	pub, _, err := GenerateKeyFromPrivateKey(private)
+	if err != nil {
+		return nil, err
+	}
+	return pub, nil
+}
+
+/******************************************************************************/
+/****************************To addresses**************************************/
+/******************************************************************************/
+
+func PublicKeyStringToFactoidAddressString(public string) (string, error) {
+	pubHex, err := hex.DecodeString(public)
+	if err != nil {
+		return "", err
+	}
+	add, err := PublicKeyToFactoidAddress(pubHex)
+	if err != nil {
+		return "", err
+	}
+
+	return factoid.ConvertFctAddressToUserStr(add), nil
+}
+
+func PublicKeyToFactoidAddress(public []byte) (factoid.IAddress, error) {
+	rcd := factoid.NewRCD_1(public)
+	add, err := rcd.GetAddress()
+	if err != nil {
+		return nil, err
+	}
+	return add, nil
 }
